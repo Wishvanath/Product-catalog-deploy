6add54ab994fbd135b3ec18511c0e9b666e1a197
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-09 11:43:07 +0530

    Updates

---
+++ precache-manifest.79c413cb27ba2edb2155f33c60555901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1d7c30e8de534aa86e92db746482c4",
+    "url": "/Product-catalog-deploy/index.html"
+  },
+  {
+    "revision": "7f43fa88b686692ed897",
+    "url": "/Product-catalog-deploy/static/css/main.d7a99429.chunk.css"
+  },
+  {
+    "revision": "8642f33d5013e7da70d7",
+    "url": "/Product-catalog-deploy/static/js/2.77d203de.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Product-catalog-deploy/static/js/2.77d203de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f43fa88b686692ed897",
+    "url": "/Product-catalog-deploy/static/js/main.1a8b53da.chunk.js"
+  },
+  {
+    "revision": "998d36492c0f732a1515",
+    "url": "/Product-catalog-deploy/static/js/runtime-main.f9db8b66.js"
+  }
+]);
